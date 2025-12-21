a69470403b35fb04c0c30841a2d30fbb6e22bedd
Anastttasia <nastya.kaneva.99@gmail.com>
2025-12-21 14:50:55 +0300

    hm9

--- budget-tracker-cli/src/interfaces/ITransaction.ts
+++ budget-tracker-cli/src/interfaces/ITransaction.ts
@@ -1,9 +1,21 @@
 import { TransactionType } from "./TransactionType";
 
 export interface ITransaction {
-  id: number;
+  id: string;
   amount: number;
   type: TransactionType;
   date: string;
-  description: string;
+  description: NullableDescription;
 }
+
+export type TransactionUpdate = Partial<ITransaction>;
+
+export type CompleteTransaction = Required<ITransaction>;
+
+export type TransactionWithoutDescription = Omit<ITransaction, 'description'>;
+
+export type TransactionPreview = Pick<ITransaction, 'id' | 'amount' | 'type' | 'date'>;
+
+export type CategoryLimits = Record<TransactionType, number>;
+
+export type NullableDescription = string | null;
